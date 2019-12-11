0acae7e0953197ad935739e5b69434ded4417fa3
ariefnur65 <ariefnur652010@gmail.com>
2019-12-11 23:16:29 +0700

    Conditional render with variable -more elegant way-

--- src/App.js
+++ src/App.js
@@ -52,6 +52,7 @@ class App extends Component{
 
 
   render () {
+    //render method always be called each time react needs to render something
     const style = {
       backgroundColor : 'white',
       font: 'inherit',
@@ -59,6 +60,28 @@ class App extends Component{
       padding: '8px',
       cursor: 'pointer'
     };
+
+    let persons = null;
+     if(this.state.showPersons){
+        persons = (
+          <div>
+            <Person 
+            name={this.state.persons[0].name} 
+            age={this.state.persons[0].age}/>        
+            <Person 
+            name={this.state.persons[1].name} 
+            age={this.state.persons[1].age}
+            click={this.switchNameHandler.bind(this, 'Axorean Bruh')}
+            change = {this.nameChangeHandler}
+            >I'm doing high-five</Person>
+            <Person 
+            name={this.state.persons[2].name} 
+            age={this.state.persons[2].age}/>
+        </div> 
+        );
+
+     }
+
     return (
     <div className="App">
       <h1>Hi, I'm a react app yo!</h1>
@@ -66,23 +89,7 @@ class App extends Component{
       <button
        style = {style}
        onClick = {this.togglePersonsHandler}>Switch Name</button>
-      {
-        this.state.showPersons === true ?   
-        <div >
-          <Person 
-          name={this.state.persons[0].name} 
-          age={this.state.persons[0].age}/>        
-          <Person 
-          name={this.state.persons[1].name} 
-          age={this.state.persons[1].age}
-          click={this.switchNameHandler.bind(this, 'Axorean Bruh')}
-          change = {this.nameChangeHandler}
-          >I'm doing high-five</Person>
-          <Person 
-          name={this.state.persons[2].name} 
-          age={this.state.persons[2].age}/>
-      </div> : null
-      }
+       {persons}
     </div>
   )
 }
